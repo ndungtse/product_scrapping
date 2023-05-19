e5428ea9d466cea9f73002f31a4ee68377078fbf
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-05-19 17:20:59 +0200

    remove dist form git

--- dist/models/Product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Product = void 0;
-const swaggiffy_1 = require("swaggiffy");
-class Product {
-}
-exports.Product = Product;
-(0, swaggiffy_1.registerSchema)('Product', Product);
